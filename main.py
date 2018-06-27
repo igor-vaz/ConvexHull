7667989857d2c978996ad4f112c14e27c2edce45
igorvaz <igor.vaz@bigdatacorp.com.br>
2018-06-26 23:16:02 -0300

    adding 2D view, need to fix projection

--- main.py
+++ main.py
@@ -1,60 +1,34 @@
-from OpenGL.GL import *
+import OpenGL 
+OpenGL.ERROR_ON_COPY = True 
+
 from OpenGL.GLUT import *
+from OpenGL.GL import *
 from OpenGL.GLU import *
-import sys
 
-# Some api in the chain is translating the keystrokes to this octal string
-# so instead of saying: ESCAPE = 27, we use the following.
-ESCAPE = '\033'
+global Points
 
-# Number of the glut window.
-window = 0
+def init2D(r,g,b):
+	glClearColor(r,g,b,0.0)    
+	glMatrixMode (GL_PROJECTION)
+	gluOrtho2D (0.0, 500.0, 0.0, 500.0)
 
-Points = []
+def display():
+	global Points
+	glClear(GL_COLOR_BUFFER_BIT)
 
-# A general OpenGL initialization function.  Sets all of the initial parameters. 
-def InitGL(Width, Height):				# We call this right after our OpenGL window is created.
-	glClearColor(0.0, 0.0, 0.0, 0.0)	# This Will Clear The Background Color To Black
-	glClearDepth(1.0)					# Enables Clearing Of The Depth Buffer
-	glDepthFunc(GL_LESS)				# The Type Of Depth Test To Do
-	glEnable(GL_DEPTH_TEST)				# Enables Depth Testing
-	glShadeModel(GL_SMOOTH)				# Enables Smooth Color Shading
-	
-	glMatrixMode(GL_PROJECTION)
-	glLoadIdentity()					# Reset The Projection Matrix
-										# Calculate The Aspect Ratio Of The Window
-	gluPerspective(45.0, float(Width)/float(Height), 0.1, 100.0)
+	glPointSize(10)
+	glColor3f(1.0, 1.0, 1.0)
+	print Points
+	if len(Points) > 0:
+		#draw two points
+		glBegin(GL_POINTS)
+		for point in Points:
+			glVertex2i(point[1],point[0])
+		glEnd()
 
-	glMatrixMode(GL_MODELVIEW)
-
-# The function called when our window is resized (which shouldn't happen if you enable fullscreen, below)
-def ReSizeGLScene(Width, Height):
-	if Height == 0:						# Prevent A Divide By Zero If The Window Is Too Small 
-		Height = 1
-
-	glViewport(0, 0, Width, Height)		# Reset The Current Viewport And Perspective Transformation
-	glMatrixMode(GL_PROJECTION)
-	glLoadIdentity()
-	gluPerspective(45.0, float(Width)/float(Height), 0.1, 100.0)
-	glMatrixMode(GL_MODELVIEW)
-
-# The main drawing function. 
-def DrawGLScene():
-	# Clear The Screen And The Depth Buffer
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-	glLoadIdentity()					# Reset The View 
-
-	glPointSize(100);
-	
-	glBegin(GL_POINTS)
-	for point in Points:
-		print "looop"
-		glColor3f(1.0, 1.0, 1.0);
-		glVertex2f(point[0],point[1])
-	glEnd()
-
-	#  since this is double buffered, swap the buffers to display what just got drawn. 
 	glutSwapBuffers()
+	#glFlush()
+
 
 # The function called whenever a key is pressed. Note the use of Python tuples to pass in: (key, x, y)  
 def keyPressed(*args):
@@ -68,7 +42,7 @@ def Upon_Click (button, button_state, cursor_x, cursor_y):
 		Glut calls this function when a mouse button is
 		clicked or released.
 	"""
-
+	global Points
 	# Mouse Scroll
 	if button == 3:
 		zoom += float(button_state)/4
@@ -78,61 +52,18 @@ def Upon_Click (button, button_state, cursor_x, cursor_y):
 	if (button == GLUT_LEFT_BUTTON and button_state == GLUT_DOWN):	
 		# Left button clicked down
 		Points.append([cursor_x, cursor_y])
-		print Points
 
-	return
-
-def main():
-	global window
-	# pass arguments to init
-	glutInit(sys.argv)
-
-	# Select type of Display mode:   
-	#  Double buffer 
-	#  RGBA color
-	# Alpha components supported 
-	# Depth buffer
-	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH)
-	
-	# get a 640 x 480 window 
-	glutInitWindowSize(640, 480)
-	
-	# the window starts at the upper left corner of the screen 
-	glutInitWindowPosition(0, 0)
-	
-	# Okay, like the C version we retain the window id to use when closing, but for those of you new
-	# to Python (like myself), remember this assignment would make the variable local and not global
-	# if it weren't for the global declaration at the start of main.
-	window = glutCreateWindow("Jeff Molofee's GL Code Tutorial ... NeHe '99")
-
-   	# Register the drawing function with glut, BUT in Python land, at least using PyOpenGL, we need to
-	# set the function pointer and invoke a function to actually register the callback, otherwise it
-	# would be very much like the C version of the code.	
-	glutDisplayFunc(DrawGLScene)
-	
-	# Uncomment this line to get full screen.
-	#glutFullScreen()
-
-	# When we are doing nothing, redraw the scene.
-	glutIdleFunc(DrawGLScene)
-	
-	# Register the function called when our window is resized.
-	glutReshapeFunc(ReSizeGLScene)
-	
-	# Register the function called when the keyboard is pressed.  
-	glutKeyboardFunc(keyPressed)
-
-	# GLUT When mouse buttons are clicked in window
-	glutMouseFunc (Upon_Click)
-
-
-	# Initialize our window. 
-	InitGL(640, 480)
-
-	# Start Event Processing Engine	
-	glutMainLoop()
-
-# Print message to console, and kick off the main to get it rolling.
-print "Hit ESC key to quit."
-main()
-		
+Points = []
+glutInit(sys.argv)
+glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB)
+glutInitWindowSize (500, 500)
+glutInitWindowPosition (100, 100)
+glutCreateWindow ('points and lines')
+# Register the function called when the keyboard is pressed.  
+glutKeyboardFunc(keyPressed)
+# GLUT When mouse buttons are clicked in window
+glutMouseFunc (Upon_Click)
+# 2D view
+init2D(0.0,0.0,0.0)
+glutDisplayFunc(display)
+glutMainLoop()
